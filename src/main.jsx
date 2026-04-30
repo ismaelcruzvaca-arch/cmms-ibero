2b4c0ca8435eac7f0f054c8866f54e0ce7a03647
Ismael Cruz <ismaelcruzvaca@gmail.com>
2026-04-30 15:46:44 -0600

    chore: integrar sentry para monitoreo de errores

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from "@sentry/react";
 import './index.css'
 import App from './App.jsx'
+
+Sentry.init({
+  dsn: "https://2fdfb99f04607fe02669a1901c18fa4e@o4511311073116160.ingest.de.sentry.io/4511311083602000",
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
